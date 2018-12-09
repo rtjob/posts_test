58882ed4143bea3aab96e36be9a3e14aa41dc470
owner <owner@owner-no-MacBook-Air1.local>
2018-12-09 14:24:42 +0900

    like消す実験

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -4,8 +4,8 @@ class LikeController < ApplicationController
 
   def create
     @like = Like.new(
-     user_id:  @current_user.id,
-     post_id: params[:id]
+     user_id:  @current_user.id
+     # post_id: params[:id]
     )
     @like.save
     redirect_to("/posts/index")
@@ -14,8 +14,8 @@ class LikeController < ApplicationController
 
   def destroy
     @like = Like.find_by(
-     user_id:  @current_user.id,
-     post_id: params[:id]
+     user_id:  @current_user.id
+     # post_id: params[:id]
     )
     @like.destroy
     redirect_to("/posts/index")
